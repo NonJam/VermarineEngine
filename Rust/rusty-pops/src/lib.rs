c26c6a8e36c37c7c8b939fc935087eab0f91bae1
Jack Stanbridge <supbscripter@gmail.com>
2020-04-16 05:10:42 +0100

    Moved project structure around

--- Rust/rusty-pops/src/lib.rs
+++
@@ -1,5 +0,0 @@
-//pub use crate::prelude::*;
-
-//pub mod prelude {
-    //pub use rpops_core::prelude::*;
-//}
--- rusty-pops/dodge-the-creeps-example/src/lib.rs
+++ rusty-pops/dodge-the-creeps-example/src/lib.rs
@@ -1,14 +1,14 @@
 mod systems;
 mod components;
-mod models;
-mod rpops_instance;
+mod renderables;
+mod state;
 
 pub use crate::prelude::*;
 
 pub mod prelude {
     pub use crate::systems::*;
     pub use crate::components::*;
-    pub use crate::models::*;
-    pub use crate::rpops_instance::*;
-    pub use rpops_engine::prelude::*;
+    pub use crate::renderables::*;
+    pub use crate::state::*;
+    pub use vermarine_engine::prelude::*;
 }
